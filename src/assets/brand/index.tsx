e02c2e2ee4a363237ef255ad56e25aba1c6fd1c4
Elia <mkumboelia@gmail.com>
2024-02-16 18:42:24 +0300

    feat: initial dashboard navbar and shadcn installation

---
+++ src/assets/brand/index.tsx
@@ -0,0 +1,11 @@
+import { CSSProperties } from "react";
+
+export type BaseIconType = {
+  size?: number;
+  className?: string;
+  style?: CSSProperties;
+};
+
+// export const NetscanLogo: React.FC<BaseIconType> = ({  }) => (
+
+// )
