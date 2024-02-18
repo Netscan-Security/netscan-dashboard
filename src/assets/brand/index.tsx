f0e62a57b0089773c9fc2a3d3519df49787a3d17
Elia <mkumboelia@gmail.com>
2024-02-18 23:24:10 +0300

    ✨ feat: added and mofied some ui components

--- src/assets/brand/index.tsx
+++ src/assets/brand/index.tsx
@@ -1,11 +1,24 @@
 import { CSSProperties } from "react";
 
+import NetscanLogo from "./netscan-logo.svg";
+
 export type BaseIconType = {
   size?: number;
   className?: string;
   style?: CSSProperties;
 };
 
-// export const NetscanLogo: React.FC<BaseIconType> = ({  }) => (
-
-// )
+export const NetscanIcon: React.FC<BaseIconType> = ({
+  size = 24,
+  className,
+  style,
+}) => {
+  return (
+    <img
+      src={NetscanLogo}
+      alt="Netscan Logo"
+      className={className}
+      style={{ width: size, height: size, ...style }}
+    />
+  );
+};
