427317cc85b14ffc38076bc1f4ec5028d544602a
Elia <mkumboelia@gmail.com>
2024-02-24 20:52:46 +0300

    refactor: file restructuring for host

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 // Local imports
 import SignInPage from "./pages/sign-in";
 import Sidebar from "./layouts/side-bar";
+import Host from "./pages/authenticated/host/host";
 import Targets from "./pages/authenticated/targets";
+import Hosts from "./pages/authenticated/host/hosts";
 import ErrorPage from "./components/templates/error";
 import Dashboard from "./pages/authenticated/dashboard";
 import NotFoundPage from "./components/templates/not-found";
 import Vulnerabilities from "./pages/authenticated/vulnerabilities";
-import Hosts from "./pages/authenticated/hosts";
-import Host from "./pages/authenticated/host";
 
 function App() {
   const router = createBrowserRouter([
