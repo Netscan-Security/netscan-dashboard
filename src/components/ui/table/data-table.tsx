dda5c10930e99738c8192a8bf577951d68f4408d
Elia <mkumboelia@gmail.com>
2024-03-05 19:13:55 +0300

    feat: improved data table component with loading

--- src/components/ui/table/data-table.tsx
+++ src/components/ui/table/data-table.tsx
@@ -7,6 +7,8 @@ import {
   getCoreRowModel,
   useReactTable,
 } from "@tanstack/react-table";
+
+// Local imports
 import {
   Table,
   TableBody,
@@ -45,9 +47,9 @@ export function DataTable<TData, TValue>({
   // debounceDuration = 400,
   className,
   headerClassname,
-}: // noDataMessage,
-// rowClassName,
-DataTableProps<TData, TValue>) {
+  rowClassName,
+  noDataMessage,
+}: DataTableProps<TData, TValue>) {
   const table = useReactTable({
     data,
     columns,
@@ -66,7 +68,25 @@ DataTableProps<TData, TValue>) {
   });
 
   return (
-    <div className="border rounded-md">
+    <div className="relative w-full min-h-[200px]">
+      {!table?.getRowModel()?.rows?.length &&
+        !(loading || pagination?.fetching) && (
+          <div className="absolute w-full h-full min-h-48">
+            <div
+              className={cn(
+                "w-[100%] h-full absolute flex justify-center items-center"
+              )}
+            >
+              {noDataMessage ?? (
+                <div className="flex flex-col items-center justify-center">
+                  <h3 className="mt-4 text-lg font-bold text-center text-slate-400">
+                    No data Found
+                  </h3>
+                </div>
+              )}
+            </div>
+          </div>
+        )}
       <Table
         className={cn(pagination?.fetching || (loading && "opacity-50"))}
         containerClassName={className}
@@ -90,11 +110,37 @@ DataTableProps<TData, TValue>) {
           ))}
         </TableHeader>
         <TableBody>
-          {table.getRowModel().rows?.length ? (
+          {loading ? (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="mx-auto animate-spin"
+                >
+                  <path d="M10.1 2.182a10 10 0 0 1 3.8 0" />
+                  <path d="M13.9 21.818a10 10 0 0 1-3.8 0" />
+                  <path d="M17.609 3.721a10 10 0 0 1 2.69 2.7" />
+                  <path d="M2.182 13.9a10 10 0 0 1 0-3.8" />
+                  <path d="M20.279 17.609a10 10 0 0 1-2.7 2.69" />
+                  <path d="M21.818 10.1a10 10 0 0 1 0 3.8" />
+                  <path d="M3.721 6.391a10 10 0 0 1 2.7-2.69" />
+                  <path d="M6.391 20.279a10 10 0 0 1-2.69-2.7" />
+                </svg>
+              </TableCell>
+            </TableRow>
+          ) : table.getRowModel().rows?.length ? (
             table.getRowModel().rows.map((row) => (
               <TableRow
                 key={row.id}
                 data-state={row.getIsSelected() && "selected"}
+                className={cn(rowClassName)}
               >
                 {row.getVisibleCells().map((cell) => (
                   <TableCell key={cell.id}>
