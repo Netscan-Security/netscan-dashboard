df6611fbd501a229c2a528ccab70e541591003cc
Fredy Mgimba <fredgerman30@gmail.com>
2024-03-18 21:37:26 +0300

    feat : updated config for docker run

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,21 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  preview: {
+    port: 8098,
+    strictPort: true,
+  },
+  server: {
+    port: 8098,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
   },
 });
